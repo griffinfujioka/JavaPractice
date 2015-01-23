8ea32ccb48b2d7200f1c43d279d0ecac589853ab
griffinfujioka <fujiokag@hotmail.com>
2015-01-22 23:20:05 -0800

    Added angularJS

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/0/00555831cfa200141c42ac018d31399b
@@ -0,0 +1,2 @@
+
+function parseRSS(url, container) { }
--- YahooNewsReader/web-app/js/YahooNewsReader.js
+++ YahooNewsReader/web-app/js/YahooNewsReader.js
@@ -1,2 +1,16 @@
 
-function parseRSS(url, container) { }
+function parseRSS(url, container) { 
+	$.ajax({
+	    url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
+	    dataType: 'json',
+	    success: function(data) {
+	      //console.log(data.responseData.feed);
+	      $(container).html('<h2>'+capitaliseFirstLetter(data.responseData.feed.title)+'</h2>');
+	 
+	      $.each(data.responseData.feed.entries, function(key, value){
+	        var thehtml = '<h3><a href="'+value.link+'" target="_blank">'+value.title+'</a></h3>';
+	        $(container).append(thehtml);
+	      });
+	    }
+	  });
+}
